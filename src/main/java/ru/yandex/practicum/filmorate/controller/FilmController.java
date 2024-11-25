f748fbc53c0a3c4c471ca6d82301057e8680335a
User Evgeniy <agentgeka@yandex.ru>
2024-11-25 14:32:16 +0500

    Исправленный коммит

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,56 +1,34 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import static ru.yandex.practicum.filmorate.exception.ValidationException.FILM_ALREADY_EXISTS;
-import static ru.yandex.practicum.filmorate.exception.ValidationException.FILM_NOT_FOUND;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
+@RequiredArgsConstructor
 public class FilmController {
 
-    Map<Long, Film> films = new HashMap<>();
-    private Long uniqId = 0L;
+    private final FilmService filmService;
 
     @GetMapping
-    public List<Film> getFimls() {
-        return new ArrayList<>(films.values());
+    public List<Film> getFilms() {
+        return filmService.getAllFilms();
     }
 
     @PostMapping
-    public Film createFilms(@Valid @RequestBody Film film) {
-        if (films.containsKey(film.getId())) {
-            throw new ValidationException(
-                    String.format(FILM_ALREADY_EXISTS, film));
-        }
-        film.setId(++uniqId);
-        films.put(film.getId(), film);
-        log.info("Успешно добавлен фильм: {}.", film);
-
-        return film;
+    public Film createFilm(@Valid @RequestBody Film film) {
+        return filmService.createFilm(film);
     }
 
     @PutMapping
-    public Film updateFilms(@Valid @RequestBody Film film) {
-        if (!films.containsKey(film.getId())) {
-            throw new ValidationException(
-                    String.format(FILM_NOT_FOUND, film));
-        }
-        films.put(film.getId(), film);
-        log.info("Фильм успешно обновлён: {}.", film);
-
-        return film;
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        return filmService.updateFilm(film);
     }
-
-
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,55 +1,34 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import static ru.yandex.practicum.filmorate.exception.ValidationException.USER_ALREADY_EXISTS;
-import static ru.yandex.practicum.filmorate.exception.ValidationException.USER_NOT_FOUND;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
-    Map<Long, User> users = new HashMap<>();
-    private Long uniqueId = 0L;
+    private final UserService userService;
 
     @GetMapping
     public List<User> getUsers() {
-        return new ArrayList<>(users.values());
+        return userService.getAllUsers();
     }
 
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
-        if (users.containsKey(user.getId())) {
-            throw new ValidationException(String.format(USER_ALREADY_EXISTS, user));
-        }
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-        user.setId(++uniqueId);
-        users.put(user.getId(), user);
-        log.info("Успешно добавлен пользователь: {}.", user);
-
-        return user;
+        return userService.createUser(user);
     }
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
-        if (!users.containsKey(user.getId())) {
-            throw new ValidationException(String.format(USER_NOT_FOUND, user));
-        }
-        users.put(user.getId(), user);
-        log.info("Пользователь успешно обновлён: {}.", user);
-
-        return user;
+        return userService.updateUser(user);
     }
 }
